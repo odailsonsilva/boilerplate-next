f32abe95b5825958f45cb8dea4beac85b7c377b6
odailson silva <odailsonsilva@MacBook-Air-de-odailson.local>
2021-05-28 14:54:49 -0300

    feat: finish boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,16 +5,8 @@ const Main = ({
   description = 'TypeScript, NextJS e StyleCompoentes'
 }) => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="um desenvolvedor de frente para tela com codigo."
-    />
   </S.Wrapper>
 )
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boilerplate</title>
+        <title>Name Project</title>
         <meta
           name="description"
           content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
